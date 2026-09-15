834d1fc2d8ca1b42a789f6436c4c15e5c5bb41df
Anna Dodson <anna.dodson@digital.cabinet-office.gov.uk>
2026-09-15 18:22:13 +0100

    Update the bootstrap script
    - Fix the GitHub repo IDs
    - Remove the permissions that are not needed

--- bootstrap/bootstrap-account.sh
+++ bootstrap/bootstrap-account.sh
@@ -3,7 +3,7 @@ set -e
 
 aws s3api create-bucket \
               --region "eu-west-2" \
-              --bucket "gds-ee-ai-coding-assistants-infra-production-tfstate" \
+              --bucket "gds-ee-ai-coding-assistants-infra-sandbox-tfstate" \
               --create-bucket-configuration LocationConstraint="eu-west-2"
 
 aws iam create-open-id-connect-provider \
@@ -12,19 +12,19 @@ aws iam create-open-id-connect-provider \
   --thumbprint-list "22ff89586561fc2d52f77491e9f1eff1b80be33e" "9514f4ed3c841c96c43def0f0acbf177405ded12" # Same as Sandbox
 
 aws iam create-role \
-    --role-name DeployPipeline-GithubActions-production-plan \
+    --role-name DeployPipeline-GithubActions-sandbox-plan \
     --assume-role-policy-document file://./assume-role.json
 
 aws iam put-role-policy \
-    --role-name DeployPipeline-GithubActions-production-plan \
-    --policy-name DeployPipeline-GithubActions-production-plan \
+    --role-name DeployPipeline-GithubActions-sandbox-plan \
+    --policy-name DeployPipeline-GithubActions-sandbox-plan \
     --policy-document file://./permissions-policy.json
 
 aws iam create-role \
-    --role-name DeployPipeline-GithubActions-production-apply \
+    --role-name DeployPipeline-GithubActions-sandbox-apply \
     --assume-role-policy-document file://./assume-role.json
 
 aws iam put-role-policy \
-    --role-name DeployPipeline-GithubActions-production-apply \
-    --policy-name DeployPipeline-GithubActions-production-apply \
+    --role-name DeployPipeline-GithubActions-sandbox-apply \
+    --policy-name DeployPipeline-GithubActions-sandbox-apply \
     --policy-document file://./permissions-policy.json
